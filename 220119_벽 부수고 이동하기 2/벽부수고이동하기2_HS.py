9a88e03398774b6367c0000e3557fc262d81aae4
hansss <ohs2251@naver.com>
2022-01-19 21:45:50 +0900

    이건 왜 틀렸을까요..

--- 220119_벽 부수고 이동하기 2/벽부수고이동하기2_HS.py
+++ 220119_벽 부수고 이동하기 2/벽부수고이동하기2_HS.py
@@ -1,11 +1,10 @@
 from collections import deque
 def solution(N, M, K):
     board = []
-    mem = []
+    mem = [[[1] * M for _ in range(N)] for __ in range(K)]
     for i in range(N):
         board.append(input())
-        mem.append([99999999]*M)
-    mem[0][0] = 1
+    mem[K-1][0][0] = 1
 
     bfs = deque([(0, 0, K, 1)]) # y좌표, x좌표, 벽 부수기, 길이
     move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -13,16 +12,15 @@ def solution(N, M, K):
         y, x, k, d = bfs.popleft()
         for my, mx in move:
             ny, nx = y + my, x + mx
-            if 0 <= ny < N and 0 <= nx < M and d+1 < mem[ny][nx]:
+            if 0 <= ny < N and 0 <= nx < M and mem[k-1][ny][nx]:
                 if board[ny][nx] == '0':
                     bfs.append((ny, nx, k, d+1))
-                    mem[ny][nx] = d+1
+                    mem[k-1][ny][nx] = 0
                 elif board[ny][nx] == '1' and k:
                     bfs.append((ny, nx, k-1, d+1))
-                    mem[ny][nx] = d+1
+                    mem[k-2][ny][nx] = 0
                 if ny == N-1 and nx == M-1:
-                    return d+1
-                    
+                    return d+1                   
 
     return -1
 
