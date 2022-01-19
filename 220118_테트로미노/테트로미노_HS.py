57c2ff8afda5f631b895349014cbd1265d98a6cf
hansss <ohs2251@naver.com>
2022-01-19 19:17:52 +0900

    add: 테트로미노_HS.py

--- 220118_테트로미노/테트로미노_HS.py
+++ 220118_테트로미노/테트로미노_HS.py
@@ -1,22 +1,23 @@
 
 def tetromino(i, j, m, mem, cnt, f=True, s=0):
+    global answer         ## 이걸로 가지치기
     
+    if answer >= s + 1000 * (4 - cnt):
+        return
     if cnt == 4:
-        return s
+        answer = max(answer, s)
+        return
 
-    result = []
     for dy, dx in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
         ny, nx = i+dy, j+dx
         if 0 <= ny < len(m) and 0 <= nx < len(m[0]) and mem[ny][nx]:
             mem[ny][nx] = 0
-            result.append(tetromino(ny, nx, m, mem, cnt+1, f, s+m[ny][nx]))
+            tetromino(ny, nx, m, mem, cnt+1, f, s+m[ny][nx])
             if f:
-                result.append(tetromino(i, j, m, mem, cnt+1, False, s+m[ny][nx]))
+                tetromino(i, j, m, mem, cnt+1, False, s+m[ny][nx])
             mem[ny][nx] = 1
-    return max(result) if result else 0
 
 def solution(N, M):
-    answer = []
     mem = [[1]*M for _ in range(N)]
     m = []
     for i in range(N):
@@ -25,12 +26,13 @@ def solution(N, M):
     for i in range(N):
         for j in range(M):
             mem[i][j] = 0
-            answer.append(tetromino(i, j, m, mem, 1, True, m[i][j]))
+            tetromino(i, j, m, mem, 1, True, m[i][j])
             mem[i][j] = 1
 
-    return max(answer)
+    return 0
 
 
 N, M = list(map(int, input().split()))
-
-print(solution(N, M))
+answer = 0
+solution(N, M)
+print(answer)
