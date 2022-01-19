03568c02e24135d87f08e03fef4b7015ef54f5fe
Junghyeon Song <gldkzm@gmail.com>
2022-01-19 11:06:12 +0900

    UPDATE: 220118 fail ㅗ 모양 블럭 만들기 필요..! 어제 커밋만 하고 푸시를 안했었네유

--- 220118_테트로미노/테트로미노_SJH.py
+++ 220118_테트로미노/테트로미노_SJH.py
@@ -1,7 +1,7 @@
 import sys
 
-def dfs(tetromino, board, length=0, max_sum = 0):
-    if length == 4:
+def dfs(tetromino, board, can_visit, max_sum):
+    if len(tetromino) == 4:
         return sum([board[i][j] for i, j in tetromino])
 
     move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -9,28 +9,30 @@ def dfs(tetromino, board, length=0, max_sum = 0):
     for m in move:
         ni = i + m[0]
         nj = j + m[1]
-
-        if not -1 < ni < len(board) or not -1 < ni < len(board[0]):
+        
+        if not -1 < ni < len(board) or not -1 < nj < len(board[0]):
             continue
 
         if (ni, nj) in tetromino:
             continue
         
         nxt_tet = tetromino[:] + [(ni, nj)]
-        length += 1
-
-        return max(max_sum, dfs(nxt_tet, board, length, max_sum))
+        can_visit[ni][nj] = False
+        max_sum = max(max_sum, dfs(nxt_tet, board, can_visit, max_sum))
     
-    return 0
+    return max_sum
 
 
 input = sys.stdin.readline
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for n in range(N)]
+can_visit = [[True for m in range(M)] for n in range(N)]
 answer = 0
 
 for i in range(N):
     for j in range(M):
-        answer = max(answer, dfs([(i, j)], board))
+        if can_visit[i][j]:
+            can_visit[i][j] = False
+            answer = max(answer, dfs([(i, j)], board, can_visit, board[i][j]))
 
 print(answer)
