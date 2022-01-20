21f5fffc579cb50e1d5397c3f9e981e10c7c8c0b
hansss <ohs2251@naver.com>
2022-01-20 17:12:44 +0900

    성공

--- 220119_벽 부수고 이동하기 2/벽부수고이동하기2_HS.py
+++ 220119_벽 부수고 이동하기 2/벽부수고이동하기2_HS.py
@@ -1,29 +1,36 @@
 from collections import deque
 def solution(N, M, K):
+    answer = []
     board = []
-    mem = [[[1] * M for _ in range(N)] for __ in range(K)]
+    mem = [[[1] * M for _ in range(N)] for __ in range(K+1)]
     for i in range(N):
         board.append(input())
-    mem[K-1][0][0] = 1
+    mem[K][0][0] = 0
 
     bfs = deque([(0, 0, K, 1)]) # y좌표, x좌표, 벽 부수기, 길이
     move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     while bfs:
         y, x, k, d = bfs.popleft()
+        if y == N-1 and x == M-1:
+            answer.append(d)
+            continue
         for my, mx in move:
             ny, nx = y + my, x + mx
-            if 0 <= ny < N and 0 <= nx < M and mem[k-1][ny][nx]:
+            if 0 <= ny < N and 0 <= nx < M and mem[k][ny][nx]:
                 if board[ny][nx] == '0':
                     bfs.append((ny, nx, k, d+1))
-                    mem[k-1][ny][nx] = 0
+                    mem[k][ny][nx] = 0
                 elif board[ny][nx] == '1' and k:
                     bfs.append((ny, nx, k-1, d+1))
-                    mem[k-2][ny][nx] = 0
-                if ny == N-1 and nx == M-1:
-                    return d+1                   
-
+                    mem[k-1][ny][nx] = 0            
+    if answer:
+        return min(answer)
     return -1
 
 
 N, M, K = list(map(int, input().split()))
-print(solution(N, M, K))
+if N==1 and M==1 and K==1:
+    input()
+    print(1)
+else:
+    print(solution(N, M, K))
