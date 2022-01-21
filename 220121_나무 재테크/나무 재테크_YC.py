b5e53edfe2f101aeff2ac95d4ea10fc0b79aaab6
YooRi Chae <yoorichae@gmail.com>
2022-01-22 01:53:20 +0900

    UPDATE: add comments

--- 220121_나무 재테크/나무 재테크_YC.py
+++ 220121_나무 재테크/나무 재테크_YC.py
@@ -22,13 +22,13 @@ for cnt in range(K):
     # spring + summer
     for r in range(N):
         for c in range(N):
-            g = ground[r][c]
+            g = ground[r][c]    # 초기 땅 양분
             for _ in range(len(trees[r][c])):
-                age = trees[r][c].popleft()
-                if g - age >= 0:
+                age = trees[r][c].popleft() # 일단 나무 뽑고
+                if g - age >= 0:    # 양분 먹을 수 있으면 양분 먹고 다시 심어줌
                     trees[r][c].append(age+1)
                     ground[r][c]-=age; g-=age                
-                else: 
+                else:   # 뽑은 나무 양분으로 만들어서 땅에 줌
                     ground[r][c] += (age//2)
 
     # fall + winter
