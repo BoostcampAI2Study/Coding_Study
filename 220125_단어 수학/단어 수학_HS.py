5b07a2492847c45b62aa2035de0aee3666cf45dc
hansss <ohs2251@naver.com>
2022-01-24 23:24:57 +0900

    add: 단어수학_HS

--- 220125_단어 수학/단어 수학_HS.py
+++ 220125_단어 수학/단어 수학_HS.py
@@ -1,24 +1,17 @@
-from collections import deque
-import heapq as hq
-from collections import deque, Counter, defaultdict
+from collections import deque, defaultdict
 
 N = int(input())
 answer = 0
 words = []
 nums = deque([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
-word_dict = dict()
-word_num = defaultdict(list)
+word_num = defaultdict(int)
 
 for i in range(N):
     tmp = input()
     for j in range(1, len(tmp)+1):
-        word_num[j].append(tmp[-j])
+        word_num[tmp[-j]] += 10**(j-1)
 
-for k, v in sorted(word_num.items(), reverse=True):
-    counter = Counter(v).most_common()
-    for c, n in counter:
-        if c not in word_dict:
-            word_dict[c] = nums.popleft()
-        answer += 10**(k-1) * word_dict[c] * n
+for v in sorted(word_num.values(), reverse=True):
+    answer += v * nums.popleft()
 
 print(answer)
