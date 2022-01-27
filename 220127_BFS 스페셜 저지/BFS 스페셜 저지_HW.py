0d703d37213801ad7c201f315d557ef0c50df7d1
hyewon11 <39722108+hyewon11@users.noreply.github.com>
2022-01-27 19:16:13 +0900

    ADD: 220127 fail

---
+++ 220127_BFS 스페셜 저지/BFS 스페셜 저지_HW.py
@@ -0,0 +1,44 @@
+import collections
+import sys
+
+N = int(input())
+tree_input = [map(int, input().split()) for _ in range(N - 1)]
+answer_order = list(map(int, input().split()))
+visit = [False] * (N + 1)
+
+# key - value: 서로 연결되어 있는 노드
+tree_info = collections.defaultdict(list)
+for node1, node2 in tree_input:
+    tree_info[node1].append(node2)
+    tree_info[node2].append(node1)
+
+# 1부터 시작하는지 확인
+if answer_order[0] != 1:
+    print(0)
+    sys.exit()
+
+# input으로 받은 정답 순서대로 딕셔너리 value 정렬
+for i in range(1, N + 1):
+    tree_info[i].sort(key=lambda x: answer_order.index(x))
+
+# BFS
+q = collections.deque()
+q.append(1)
+visit[1] = True
+result = [1]
+while q:
+    node = q.popleft()
+    adjacency_nodes = tree_info[node]
+    child_nodes = []
+    # 방문하지 않은 자식 저장
+    for adj_node in adjacency_nodes:
+        if visit[adj_node] is False:
+            child_nodes.append(adj_node)
+            result.append(adj_node)
+    # 방문하지 않은 자식 q에 넣기
+    for child_node in child_nodes[::-1]:
+        if visit[child_node] == False:
+            q.append(child_node)
+            visit[child_node] = True
+
+print(1) if result == answer_order else print(0)
