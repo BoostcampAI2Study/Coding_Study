9c4fcf2eaf96ef15bccd0c23e7fce34d13823f0c
hyewon11 <39722108+hyewon11@users.noreply.github.com>
2022-02-17 19:02:35 +0900

    UPDATE : 220119 solution

--- 220119_벽 부수고 이동하기 2/벽 부수고 이동하기2_HW.py
+++ 220119_벽 부수고 이동하기 2/벽 부수고 이동하기2_HW.py
@@ -7,23 +7,22 @@ visited = [[[0] * (k+1) for _ in range(m)] for j in range(n)]
 q = collections.deque()
 q.append((0, 0, k))
 visited[0][0][k] = 1
+result = -1
 
 while q:
-    x, y, nk = q.popleft()
-
-    for nx, ny in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
-        nx, ny = nx + x, ny + y
-        if nx < 0 or nx >= n or ny < 0 or ny >= m or visited[nx][ny][nk] != 0:
+    r, c, nk = q.popleft()
+    if r == n-1 and c == m-1:
+        result = visited[r][c][nk]
+        break
+    for nr, nc in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
+        nr, nc = nr + r, nc + c
+        if nr < 0 or nr >= n or nc < 0 or nc >= m:
             continue
-        if nk > 0 and board[nx][ny] == 1:
-            q.append((nx, ny, nk - 1))
-            visited[nx][ny][nk - 1] = visited[x][y][nk] + 1
-        elif board[nx][ny] == 0:
-            q.append((nx, ny, nk))
-            visited[nx][ny][nk] = visited[x][y][nk] + 1
+        if nk > 0 and board[nr][nc] == 1 and visited[nr][nc][nk - 1] == 0:
+            q.append((nr, nc, nk - 1))
+            visited[nr][nc][nk - 1] = visited[r][c][nk] + 1
+        elif board[nr][nc] == 0 and visited[nr][nc][nk] == 0:
+            q.append((nr, nc, nk))
+            visited[nr][nc][nk] = visited[r][c][nk] + 1
 
-result = 1e9
-for i in visited[n - 1][m - 1]:
-    if 0 < i < result:
-        result = i
-print(result) if result != 1e9 else print(-1)
+print(result)
