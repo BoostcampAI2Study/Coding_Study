4fc7108ef220bfc473d9d7414f15309a3ffa62f9
조범준 <mitcbj@daum.net>
2022-02-17 23:51:46 +0900

    UPDATE : 220119 solution

--- 220119_벽 부수고 이동하기 2/벽부수고이동하기2_JBJ.py
+++ 220119_벽 부수고 이동하기 2/벽부수고이동하기2_JBJ.py
@@ -1,32 +1,32 @@
 import collections, sys
 
 N, M, K = map(int, sys.stdin.readline().strip().split())
-MAP = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(N)]
+MAP = [sys.stdin.readline().strip() for _ in range(N)]
 
 def bfs():
     global N, M, K, MAP
     Q = collections.deque()
-    min_distances = [[[0] * (K+1) for _ in range(M)] for _ in range(N)]
+    min_distances = [[[-1]*M for _ in range(N)] for _ in range(K+1)] # speed optimization: (N, M, K) → (K, N, M)
     
-    Q.append((0, 0, 0))          # y, x, broken walls count
-    min_distances[0][0][0] = 1   # N, M, K
+    Q.append((0, 0, 0))          # broken walls count, y, x
+    min_distances[0][0][0] = 1   # K, N, M
 
     while Q:
-        y, x, broken_walls = Q.popleft()
+        broken_walls, y, x = Q.popleft()
 
         if y == N-1 and x == M-1:
-            return min_distances[y][x][broken_walls]
+            return min_distances[broken_walls][y][x]
 
-        for new_y, new_x in ((y, x+1), (y, x-1), (y+1, x), (y-1, x)):
+        for new_y, new_x in (y, x+1), (y, x-1), (y+1, x), (y-1, x):
             if 0 <= new_y < N and 0 <= new_x < M:
-                # wall exists and K count remains. → break wall
-                if MAP[new_y][new_x] and broken_walls < K and not min_distances[new_y][new_x][broken_walls+1]:
-                    min_distances[new_y][new_x][broken_walls+1] = min_distances[y][x][broken_walls] + 1
-                    Q.append((new_y, new_x, broken_walls+1))
-                # no wall
-                elif not MAP[new_y][new_x] and not min_distances[new_y][new_x][broken_walls]:
-                    min_distances[new_y][new_x][broken_walls] = min_distances[y][x][broken_walls] + 1
-                    Q.append((new_y, new_x, broken_walls))
+                if MAP[new_y][new_x] == '1': # wall exists and K count remains. → break wall
+                    if broken_walls < K and min_distances[broken_walls+1][new_y][new_x] == -1:
+                        min_distances[broken_walls+1][new_y][new_x] = min_distances[broken_walls][y][x] + 1
+                        Q.append((broken_walls+1, new_y, new_x))
+                else: # no wall                
+                    if min_distances[broken_walls][new_y][new_x] == -1:
+                        min_distances[broken_walls][new_y][new_x] = min_distances[broken_walls][y][x] + 1
+                        Q.append((broken_walls, new_y, new_x))
     return -1
 
 print(bfs())
