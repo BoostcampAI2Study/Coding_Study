6b991c21af222d004493557de4f347cf660da172
hyewon11 <39722108+hyewon11@users.noreply.github.com>
2022-04-08 04:05:19 +0900

    UPDATE: 220110 solution

--- 220110_이모티콘/이모티콘_HW.py
+++ 220110_이모티콘/이모티콘_HW.py
@@ -1,24 +1,23 @@
-def solution(S):
-    queue = [(1, 2, 2)]
-    result = 0
-    visit = [[0 for i in range(1000)] for j in range(1000)]
-    visit[0][1] = 1
-    while queue:
-        clip, tmp, time = queue.pop(0)
-        if tmp == S:
-            result = time
-            break
-        # copy
-        if visit[tmp-1][tmp-1]==0:
-            queue.append((tmp,tmp,time+1))
-            visit[tmp - 1][tmp - 1] = 1
-        # paste
-        if (clip + tmp) <= 1000 and visit[clip-1][clip+tmp-1]==0:
-            queue.append((clip,clip+tmp,time+1))
-            visit[clip - 1][clip + tmp - 1] = 1
-        # delete
-        if tmp > 1 and visit[clip-1][tmp-2]==0:
-            queue.append((clip,tmp-1,time+1))
-            visit[clip - 1][tmp - 2] = 1
-
-    return result
+import sys, collections
+S = int(sys.stdin.readline())
+q = collections.deque()
+q.append((1, 1))
+visit = [[0] * S for _ in range(S)]
+visit[0][0] = 1
+while q:
+    clip, screen = q.popleft()
+    if screen == S:
+        print(visit[clip - 1][screen - 1])
+        break
+    # copy
+    if not visit[screen - 1][screen - 1]:
+        q.append((screen, screen))
+        visit[screen - 1][screen - 1] = visit[clip - 1][screen - 1] + 1
+    # paste
+    if (clip + screen) <= S and not visit[clip - 1][clip + screen - 1]:
+        q.append((clip, clip + screen))
+        visit[clip - 1][clip + screen - 1] = visit[clip - 1][screen - 1] + 1
+    # delete
+    if clip >= 1 and screen >= 2 and not visit[clip - 1][screen - 2]:
+        q.append((clip, screen - 1))
+        visit[clip - 1][screen - 2] = visit[clip - 1][screen - 1] + 1
