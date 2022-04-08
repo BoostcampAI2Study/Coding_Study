fe76a3d18e5e1ae3e1a8174d4b650d2fff1c712a
조범준 <mitcbj@daum.net>
2022-04-08 12:19:08 +0900

    UPDATE : 220118 solution

--- 220118_테트로미노/테트로미노_JBJ.py
+++ 220118_테트로미노/테트로미노_JBJ.py
@@ -2,28 +2,32 @@ import sys, heapq
 
 N, M = map(int, sys.stdin.readline().strip().split())
 BOARD = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
-max_weights_sum = 0
+max_weight_sum, MAX_WEIGHT = 0, max(map(max, BOARD))
+visited = [[False]*M for _ in range(N)]
 
-# BF
-for i in range(N):
-    for j in range(M):
-        heap = [(-BOARD[i][j], i, j)] # min heap -> max heap
-        is_visited = set()
-        weights_sum = 0
+def dfs(y, x, square_cnt, weight_sum):
+    global max_weight_sum, MAX_WEIGHT, N, M, BOARD, visited
 
-        # greedy
-        while heap:
-            weight, cur_i, cur_j = heapq.heappop(heap)
-            weights_sum -= weight
-            is_visited.add((cur_i, cur_j))
+    if max_weight_sum >= weight_sum + (MAX_WEIGHT * (4 - square_cnt)): return # backtracking
 
-            # tetromino check
-            if len(is_visited) == 4: break
+    if square_cnt == 4:
+        max_weight_sum = max(max_weight_sum, weight_sum)
+        return
 
-            for new_i, new_j in ((cur_i, cur_j+1), (cur_i, cur_j-1), (cur_i+1, cur_j), (cur_i-1, cur_j)):
-                if 0 <= new_i < N and 0 <= new_j < M and (new_i, new_j) not in is_visited:
-                    heapq.heappush(heap, (-BOARD[new_i][new_j], new_i, new_j))
+    for new_y, new_x in ((y, x+1), (y, x-1), (y+1, x), (y-1, x)):
+        if 0 <= new_y < N and 0 <= new_x < M and not visited[new_y][new_x]:
+            if square_cnt == 2: # to make 'ㅗ, ㅜ' tetrominos.
+                visited[new_y][new_x] = True
+                dfs(y, x, square_cnt+1, weight_sum+BOARD[new_y][new_x])
+                visited[new_y][new_x] = False
+            visited[new_y][new_x] = True
+            dfs(new_y, new_x, square_cnt+1, weight_sum+BOARD[new_y][new_x])
+            visited[new_y][new_x] = False
 
-        max_weights_sum = max(max_weights_sum, weights_sum)
+for y in range(N):
+    for x in range(M):
+        visited[y][x] = True
+        dfs(y, x, 1, BOARD[y][x])
+        visited[y][x] = False
 
-print(max_weights_sum)
+print(max_weight_sum)
