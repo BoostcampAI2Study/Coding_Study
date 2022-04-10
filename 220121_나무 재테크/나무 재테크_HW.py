ea090406dc936c424a551c84f214e0c8c47f57a5
hyewon11 <39722108+hyewon11@users.noreply.github.com>
2022-04-10 21:41:30 +0900

    UPDATE: 220121 solution

--- 220121_나무 재테크/나무 재테크_HW.py
+++ 220121_나무 재테크/나무 재테크_HW.py
@@ -1,42 +1,44 @@
-N, M, K = map(int, input().split())
-A = [[int(i) for i in input().split()] for _ in range(N)]
-tree_info = [[int(i) for i in input().split()] for _ in range(M)]
-tree_info = [[r - 1, c - 1, age] for r, c, age in tree_info]
+import sys, collections
+N, M, K = map(int, sys.stdin.readline().split())
+A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+tree_info = [[collections.deque() for _ in range(N)] for _ in range(N)]
+for _ in range(M):
+    r, c, age = map(int, sys.stdin.readline().split())
+    tree_info[r - 1][c - 1].append(age)
 
 soil_info = [[5] * N for _ in range(N)]
-cur_tree, new_tree, die_tree = [], [], []
+tree_cnt = M
 NR = [-1, -1, -1, 0, 0, 1, 1, 1]
 NC = [-1, 0, 1, -1, 1, -1, 0, 1]
 
-tree_info.sort()
 for year in range(K):
-    for idx in range(len(tree_info)):
-        r, c, age = tree_info[idx]
-        # 봄
-        if soil_info[r][c] >= age:
-            soil_info[r][c] -= age
-            cur_tree.append([r, c, age + 1])
-            # 새로운 나무
-            if (age+1) % 5 == 0:
-                for i in range(8):
-                    nr, nc = NR[i] + r, NC[i] + c
-                    if nr < 0 or nr >= N or nc < 0 or nc >= N:
-                        continue
-                    new_tree.append([nr, nc, 1])
-        else:
-            die_tree.append([r, c, age//2])         # 나무 죽음
+    for r in range(N):
+        for c in range(N):
+            dead_tree = 0
+            # spring
+            for i in range(len(tree_info[r][c])):
+                age = tree_info[r][c].popleft()
+                if age <= soil[r][c]:
+                    tree_info[r][c].append(age + 1)
+                    soil[r][c] -= age
+                else:
+                    dead_tree += age // 2
+                    tree_cnt -= 1
+            # summer
+            soil[r][c] += dead_tree
 
-    tree_info = cur_tree[:]
-    # 여름
-    for r, c, age in die_tree:
-        soil_info[r][c] += age
-    # 새나무 심기
-    tree_info = new_tree + tree_info
-    # 겨울
-    soil_info = [[soil_info[r][c] + A[r][c] for c in range(N)] for r in range(N)]
+    for r in range(N):
+        for c in range(N):
+            # autumn
+            for age in tree_info[r][c]:
+                if age % 5 == 0:
+                    for i in range(8):
+                        nr, nc = r + NR[i], c + NC[i]
+                        if 0 <= nr < N and 0 <= nc < N:
+                            tree_info[nr][nc].appendleft(1)
+                            tree_cnt += 1
+            # winter
+            soil[r][c] += A[r][c]
 
-    cur_tree.clear()
-    die_tree.clear()
-    new_tree.clear()
-
-print(len(tree_info))
+print(tree_cnt)
