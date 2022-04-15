bc2e735717815169c67d928d2d7dc3cef888c48a
조범준 <mitcbj@daum.net>
2022-04-15 13:23:36 +0900

    UPDATE : 220509 solution

--- 220509_차량 번호판1/차량번호판1_JBJ.py
+++ 220509_차량 번호판1/차량번호판1_JBJ.py
@@ -1,5 +1,5 @@
 plate_cnt, prev_char = 1, ''
-for cur_char in input().rstrip():
+for cur_char in input():
     case = 26 if cur_char == 'c' else 10
     plate_cnt *= (case - 1) if cur_char == prev_char else case
     prev_char = cur_char
